509e0e0505daf06f9fc2952390a5b9e3a6c5bafe
Jarom Rial <jarom.rial@gmail.com>
2021-01-08 13:12:00 -0700

    chore: add Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      }
+    },
   },
   variants: {
     extend: {},
