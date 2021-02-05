7eac9bc0e90b10d1533bb3490c2f5cffe0846fd3
Jarom Rial <jarom.rial@gmail.com>
2021-02-05 13:11:39 -0700

    add configurations

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: ['gatsby-plugin-postcss'],
+  siteMetadata: {
+    // title: "Jarom Rial Web Development",
+    titleTemplate: "%s - Jarom Rial Professional Web Development",
+    description: "Need a website? I'm a professional web developer. Visit my website and let's get working a website for you business.",
+    url: "https://jarom.dev",
+  },
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
+  ],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,37 @@
 const { fontFamily } = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      colors: {
+        'light-blue': colors.lightBlue,
+        orange: colors.orange,
+      },
       fontFamily: {
+        logo: ['Roboto Slab'],
         sans: ['Inter', ...fontFamily.sans],
-      }
+      },
+      maxWidth: {
+        'xxs': '18rem',
+      },
+      spacing: {
+        '88': '22rem',
+        '128': '32rem',
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio')
+  ],
 }
