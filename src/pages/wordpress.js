fa8ffc53e45f80e42348e7d2bce565329ec95a73
Jarom Rial <jarom.rial@gmail.com>
2021-04-01 14:38:56 -0600

    add basic page component structure

--- src/pages/wordpress.js
+++ src/pages/wordpress.js
@@ -1,3 +1,15 @@
 import React from "react";
+import SEO from "../components/seo";
+import MainLayout from "../components/layouts/Layout";
 
-export default function Wordpress() {}
+export default function Wordpress() {
+    return (
+        <>
+            <SEO title="Wordpress" />
+
+            <MainLayout>
+                
+            </MainLayout>
+        </>
+    )
+}
