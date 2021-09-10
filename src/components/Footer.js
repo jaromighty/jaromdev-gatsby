542b7e677bfb85cc5c093840092b4feaceb88bfa
Jarom Rial <jarom.rial@gmail.com>
2021-09-10 11:57:30 -0600

    display current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
                 </div>
                 <div className="mt-8 md:mt-0 md:order-1">
                     <p className="text-center text-base text-gray-400">
-                        &copy; 2020 Jarom Rial. All rights reserved.
+                        &copy; { new Date().getFullYear() } Jarom Rial. All rights reserved.
                     </p>
                 </div>
             </div>
