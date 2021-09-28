6278b34f2ac1e2259f0202b58821209b9ccaf96d
Jarom Rial <jarom.rial@gmail.com>
2021-09-28 06:42:19 -0600

    display robots noinidex meta tag

--- src/pages/refresh/orchard-chiropractic.js
+++ src/pages/refresh/orchard-chiropractic.js
@@ -12,6 +12,7 @@ export default function OrchardChiropractic() {
         <>
             <SEO
                 title="Orchard Chiropractic"
+                robots={true}
             />
 
             <RefreshBanner color="bg-red-600" />
