c05c23b0385bc86454c25994204fdbd203439241
Jarom Rial <jarom.rial@gmail.com>
2021-10-23 12:50:46 -0600

    add the gaming husband to portfolio

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -6,9 +6,11 @@ import countryk9 from "../../images/portfolio-websites/countryk9.jpg";
 import harpistsprings from "../../images/portfolio-websites/harpistinthesprings.jpg";
 import boers from "../../images/portfolio-websites/mount-lewis-boers.jpg";
 import relativeinterest from "../../images/portfolio-websites/relativeinterest.jpg";
+import gaminghusband from "../../images/portfolio-websites/the-gaming-husband.jpg";
 
 const clients = [
     { name: "Country K9", href: "https://countryk9.dog/?ref=jaromdev", image: countryk9 },
+    { name: "The Gaming Husband", href: "https://gaminghusband.com/?ref=jaromdev", image: gaminghusband },
     { name: "Harpist in the Springs", href: "https://harpistinthesrpings.com/?ref=jaromdev", image: harpistsprings },
     { name: "Mount Lewis Boers", href: "https://mountlewisboers.com/?ref=jaromdev", image: boers },
     { name: "Relative Interest", href: "https://relativeinterest.com/?ref=jaromdev", image: relativeinterest },
@@ -28,7 +30,7 @@ export default function Portfolio() {
                         <div className="pb-5 border-b border-gray-200">
                             <h3 className="text-lg leading-6 font-medium text-gray-900">Client Websites</h3>
                         </div>
-                        <div className="mt-8 grid grid-cols-1 gap-y-8 sm:grid-cols-3 sm:gap-x-5 sm:gap-y-6 md:grid-cols-4 lg:col-span-3">
+                        <div className="mt-8 grid grid-cols-1 gap-y-8 sm:grid-cols-3 sm:gap-x-5 md:grid-cols-4 lg:col-span-3">
                             {clients.map(client => (
                                 <a href={client.href} target="_blank" rel="noreferrer" className="block group">
                                     <figure>
